031a1350aabed9fdb59bdfab77495c89cab54b56
adnan-shaikh23242324 <107952487+adnan-shaikh23242324@users.noreply.github.com>
2023-04-04 16:19:26 +0530

    added validation

--- src/app/create-registration/create-registration.component.ts
+++ src/app/create-registration/create-registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 import { NgToastService } from 'ng-angular-popup';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -11,6 +11,7 @@ import { User } from '../models/user.model';
   styleUrls: ['./create-registration.component.scss']
 })
 export class CreateRegistrationComponent implements OnInit {
+[x: string]: any;
   public packages: string[] = ["Monthly", "Quarterly", "Yearly"];
   public genders: string[] = ["Male", "Female"];
   public importantList: string[] = [
@@ -21,18 +22,18 @@ export class CreateRegistrationComponent implements OnInit {
     "Sugar Craving Body",
     "Fitness"];
   public registerForm!: FormGroup;
-  public userIdToUpdate!:number;
-  public isUpdateActive:boolean=false;
+  public userIdToUpdate!: number;
+  public isUpdateActive: boolean = false;
 
-  constructor(private fb: FormBuilder,private activatedRoute: ActivatedRoute,private router:Router,private api:ApiService,private toastservice:NgToastService) {
+  constructor(private fb: FormBuilder, private activatedRoute: ActivatedRoute, private router: Router, private api: ApiService, private toastservice: NgToastService) {
 
   }
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      email: [''],
-      mobile: [''],
+      firstName: ['', Validators.required],
+      lastName: ['',Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.minLength(10)]],
       weight: [''],
       height: [''],
       bmi: [''],
@@ -46,37 +47,39 @@ export class CreateRegistrationComponent implements OnInit {
 
 
     });
-    this.registerForm.controls['height'].valueChanges.subscribe(res=>{
+    this.registerForm.controls['height'].valueChanges.subscribe(res => {
       this.calculateBmi(res);
     });
-    this.activatedRoute.params.subscribe(val=>{
-      this.userIdToUpdate=val['id'];
+    this.activatedRoute.params.subscribe(val => {
+      this.userIdToUpdate = val['id'];
       this.api.getRegisteredUserTd(this.userIdToUpdate)
-      .subscribe(res=>{
-        this.isUpdateActive=true;
-        this.fillFormToUpdate(res);
+        .subscribe(res => {
+          this.isUpdateActive = true;
+          this.fillFormToUpdate(res);
 
-      })
+        })
     })
   }
   submit() {
-    this.api.postRegistration(this.registerForm.value)
-    .subscribe(res=>{
-      this.toastservice.success({detail:"Sucess",summary:"Enqiury Added",duration:3000});
-      this.registerForm.reset();
+    if (this.registerForm.valid) {
+      this.api.postRegistration(this.registerForm.value)
+        .subscribe(res => {
+          this.toastservice.success({ detail: "Sucess", summary: "Enqiury Added", duration: 3000 });
+          this.registerForm.reset();
 
-    })
+        })
+    }
 
   }
-  update(){
+  update() {
 
-    this.api.updateRegisteredUser(this.registerForm.value,this.userIdToUpdate)
-    .subscribe(res=>{
-      this.toastservice.success({detail:"Sucess",summary:"Enqiury Added",duration:3000});
-      this.registerForm.reset();
-      this.router.navigate(['list'])
+    this.api.updateRegisteredUser(this.registerForm.value, this.userIdToUpdate)
+      .subscribe(res => {
+        this.toastservice.success({ detail: "Sucess", summary: "Enqiury Added", duration: 3000 });
+        this.registerForm.reset();
+        this.router.navigate(['list'])
 
-    })
+      })
   }
   calculateBmi(heightValue: number) {
     const weight = this.registerForm.value.height;
@@ -84,15 +87,15 @@ export class CreateRegistrationComponent implements OnInit {
     const bmi = weight / (height * height);
     this.registerForm.controls['bmi'].patchValue(bmi);
     switch (true) {
-      case (bmi < 18.5):
+      case bmi < 18.5:
         this.registerForm.controls['bmiResult'].patchValue("Underweight");
         break;
 
-      case (bmi>=18.5 && bmi<25):
+      case (bmi >= 18.5 && bmi < 25):
         this.registerForm.controls['bmiResult'].patchValue("Normal");
         break;
 
-      case (bmi>=25 && bmi<30):
+      case (bmi >= 25 && bmi < 30):
         this.registerForm.controls['bmiResult'].patchValue("Overweight");
         break;
 
@@ -105,7 +108,7 @@ export class CreateRegistrationComponent implements OnInit {
     }
 
   }
-  fillFormToUpdate(user:User){
+  fillFormToUpdate(user: User) {
     this.registerForm.patchValue({
       firstName: user.firstName,
       lastName: user.lastName,
